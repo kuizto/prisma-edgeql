fce291643a7f43154f8213f37c0e4793c356e84a
Sylvain <hello@sylvainsimao.fr>
2023-05-17 21:36:45 +1000

    cleanup

--- dist/gql.d.ts
+++
@@ -1,7 +0,0 @@
-export default class GraphQL {
-    select: any | null;
-    where: any | null;
-    data: any | null;
-    constructor(query: string, args?: any);
-}
-export declare function parse(key: 'select' | 'data' | 'where', queryArgs?: any): any | null;
